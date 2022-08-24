35ca8c43cdd5210090bfb47da3a674a21a5850bf
Yuriy Kozak <zoumzgame@gmail.com>
2022-08-24 15:13:04 +0300

    Add my app.js

--- js/app.js
+++ js/app.js
@@ -1,39 +1,96 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Player {
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    constructor(x, y, score = 0){
+        this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+        this.score = score;
+    }
+
+    update () {
+
+        if (this.y < 0){
+            setTimeout(() => {
+                this.x = 200;
+                this.y = 400;
+            }, 300)
+            // this.score += 1;
+            // document.querySelector('.score').innerHTML = `Score: ${this.score}`;
+            // console.log(this.score);
+        }
+    }
+    render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    handleInput(key){
+       if (key == 'left' && this.x > 0){
+        this.x -= 101;
+       }
+       if (key == 'right' && this.x < 400){
+        this.x += 101;
+       }
+       if (key == 'up' && this.y > 0){
+        this.y -= 82;
+       }
+       if (key == 'down' && this.y < 400){
+        this.y += 82;
+       }
+    }
+}
+
+let player = new Player(200,400);
+
+class Enemy {
+
+    constructor(x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = this.changeSpeed();
+    }
+
+    changeSpeed(min = 200, max = 400) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    enemyKillPlayer() {
+        if(player.x < this.x + 80 &&
+            player.x + 80 > this.x &&
+            player.y < this.y + 60 &&
+            60 + player.y > this.y &&
+            60 + player.y > this.y){
+                player.x = 200;
+                player.y = 400;
+        }
+    }
+
+    update(delta) {
+        this.x += this.speed * delta; 
+
+        if(this.x > 505){
+            this.speed = this.changeSpeed();
+            this.x = -100;
+        }
+
+        this.enemyKillPlayer();
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+let allEnemies = [];
+let yForEnemies = [50, 140, 225];
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+yForEnemies.forEach((valueY) => {
+    enemy = new Enemy(-100, valueY);
+    allEnemies.push(enemy);
+})
+console.log(allEnemies);
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
